abc28e51caeee9c9d11f8e097df54fb60616e307
alexandrebk <alexandre.b2506@gmail.com>
2018-03-18 10:24:22 +0100

    add comments

--- recursive.rb
+++ recursive.rb
@@ -1,16 +1,16 @@
 def merge_and_sort(array)
-  # fonction pour ordonner un tableau dans l'ordre décroissant
+  # Fonction qui ordonne un tableau dans l'ordre décroissant
   if array.size == 1
     return array
   else
-    # on divise le tableau en 2 - étape 1
+    # On divise le tableau en 2 - étape 1
     half_size = array.size / 2
     first_half = array[0...half_size]
     second_half = array[half_size..-1]
-    # on trie chaque tableau en appelant récursivement la fonction merge_and_sort - étape 2
+    # On trie chaque tableau en appelant récursivement la fonction merge_and_sort - étape 2
     first_half_sorted = merge_and_sort(first_half)
     second_half_sorted = merge_and_sort(second_half)
-    # on fusionne et on trie les tableaux en comparant un à un les éléments de chaque tableau - étape 3
+    # On fusionne et on trie les tableaux en comparant un à un les éléments de chaque tableau - étape 3
     array_rearanged = sort(first_half_sorted,second_half_sorted)
   end
 end
@@ -19,12 +19,16 @@ def sort(arr_1,arr_2)
   # si les tableaux ont chacun une valeur alors on les trie dans l'ordre décroissant.
   if arr_1.size == 1 && arr_2 == 1
     arr_1 > arr_2 ? [arr_2, arr_1] : [arr_1 , arr_2]
-  # sinon on itère sur les tableaux. On va comparer les valeurs une a une des tableaux et on les range dans un tableau final
+  # Sinon on itère en parallèle sur les deux tableaux.
+  # On compare la valeur en cours (i) d'un tableau avec la valeur en cours (j) de l'autre tableau.
+  # La plus grande valeur est mise dans le tableau final.
+  # Ensuite on incrémente la valeur en cours pour le tableau qui contenait la valeur la plus grande (i+1 ou j+1).
+  # Et on recommence la comparaison.
   else
     i = 0
     j = 0
     final_array = []
-    # tant qu'on a pas atteint la fin des 2 tableaux on continue itérer.
+    # Tant qu'on a pas atteint la fin des 2 tableaux on continue itérer.
     while i <= arr_1.size && j <= arr_2.size
       if arr_1[i] == nil
         final_array << arr_2[j] if arr_2[j] != nil
