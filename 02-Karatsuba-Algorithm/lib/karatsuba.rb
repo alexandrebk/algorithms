3af224f1f84dc12e46148fbf288ce83f9f5d0de3
alexandrebk <alexandre.b2506@gmail.com>
2018-07-22 18:05:15 +0200

    exerice 2: resolution

--- 02-Karatsuba-Algorithm/lib/karatsuba.rb
+++ 02-Karatsuba-Algorithm/lib/karatsuba.rb
@@ -1,11 +1,12 @@
 def multiplication(integer1, integer2 )
 
   # mettre un if on retourn juste la multiplication si c'est la taille des 2 est de 1
-  return integer1 * integer2 if integer1 < 10 && integer2 < 10
+  puts "NOUVELLE BOUCLE AVEC #{integer1} et #{integer2}"
+  return integer1 * integer2 if integer1 < 10 || integer2 < 10
   split_integer1 = divide_an_integer(integer1)
   split_integer2 = divide_an_integer(integer2)
-  n = split_integer1.length
-  puts "Taille de la raie? #{n}"
+  n = split_integer1[2]
+  puts "Taille de la raie? #{split_integer1[2]}"
   a = split_integer1[0]
   puts "a: #{a}"
   b = split_integer1[1]
@@ -18,14 +19,15 @@ def multiplication(integer1, integer2 )
   # Il faut calculer b fois d
   # Il faut calculer a fois c
   premier   = multiplication(a,c)# a * c
-  puts "premier: #{premier*10**n}"
+  puts "premier: #{premier}"
   second    = multiplication(b,d)# b * d
   puts "second: #{second}"
   troisieme = multiplication(a+b,c+d)# (a + b) * (c + d)
   puts "troisieme: #{troisieme}"
   quatrieme = troisieme - premier - second
-  puts "quatrieme: #{quatrieme*10**(n/2)}"
-  puts "Est-ce qu'on a 7006652?"
+  puts "quatrieme: #{quatrieme}"
+  puts "result"
+  p premier*10**n + 10**(n/2)*quatrieme + second
   return premier*10**n + 10**(n/2)*quatrieme + second
 end
 
@@ -34,12 +36,15 @@ def divide_an_integer(integer)
   half_size   = array.size / 2
   first_half  = array[0...half_size]
   second_half = array[half_size..-1]
-  p [first_half.join.to_i, second_half.join.to_i]
+  p [first_half.join.to_i, second_half.join.to_i, array.size]
 end
 
 number1 = 3141592653589793238462643383279502884197169399375105820974944592
 number2 = 2718281828459045235360287471352662497757247093699959574966967627
-number3 = 1234
-number4 = 5678
+number3 = 12341234
+number4 = 56785678
+
 
 p multiplication(number3, number4)
+
+p number3 * number4
