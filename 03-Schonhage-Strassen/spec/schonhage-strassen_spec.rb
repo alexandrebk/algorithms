c628be1a954c15b1d6f6a044e8b8d51c7dee1b24
alexandrebk <alexandre.b2506@gmail.com>
2018-07-24 14:03:10 +0200

    03 Schonhage Strassen spec

--- 03-Schonhage-Strassen/spec/schonhage-strassen_spec.rb
+++ 03-Schonhage-Strassen/spec/schonhage-strassen_spec.rb
@@ -3,19 +3,31 @@ require_relative "../lib/schonhage-strassen"
 number1 = 12341234
 number2 = 56785678
 
-describe "Should return an integer." do
+describe "Basic Test. " do
 
-  it "should return an integer." do
-    expect( multiplication(number1, number2) ).to be_a integer
+  it "Should return an integer." do
+    expect( multiplication(number1, number2) ).to be_a Integer
     # expect( sort(array_of_integer).class.to_s ).to eq("Array")
+  end
+
+  it "Should return the product of arguments." do
+    # expect( sort(array_of_integer) ).to eq(answer)
+    expect( multiplication(12, 34) ).to eq(408)
+    expect( multiplication(number1, number2) ).to eq(number1 * number2)
   end
 
 end
 
-describe "Should return an product of two integer." do
+describe "Inside the function. " do
 
-  it "Should return an product of two integer." do
+  it "Should split arguments in one digit number array." do
+    expect( multiplication(number1, number2) ).to be_a Integer
+    # expect( sort(array_of_integer).class.to_s ).to eq("Array")
+  end
+
+  it "Should make two array iteration with each_with_index." do
     # expect( sort(array_of_integer) ).to eq(answer)
+    expect( multiplication(12, 34) ).to eq(408)
     expect( multiplication(number1, number2) ).to eq(number1 * number2)
   end
 
